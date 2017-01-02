ae1f587642d9e5fdd1cdcfa96e4364351c8c9c2a
Nik <nik.kholin@gmail.com>
2017-01-02 23:21:28 +0200

    Add letter movement

--- js/app.js
+++ js/app.js
@@ -4,7 +4,8 @@ triviaApp.controller("QuestionsController", function QuestionsController($scope)
   var welcome_question = {
     id: 0,
     answer: "Yes",
-    answerShuffledLetters: ["Y", "E", "S"],
+    answerUsedLetters: [],
+    answerUnusedLetters: ["Y", "E", "S"],
     question: "Are you ready to have some fun?",
     category: "Fun"
   };
@@ -12,4 +13,14 @@ triviaApp.controller("QuestionsController", function QuestionsController($scope)
   $scope.answers_total = 0;
   $scope.answers_correct = 0;
   $scope.question = welcome_question
+
+  $scope.useLetter = function(letter_index) {
+    var letter = $scope.question.answerUnusedLetters.splice(letter_index, 1)[0];
+    $scope.question.answerUsedLetters.push(letter);
+  }
+
+  $scope.unuseLetter = function(letter_index) {
+    var letter = $scope.question.answerUsedLetters.splice(letter_index, 1)[0];
+    $scope.question.answerUnusedLetters.push(letter);
+  }
 });
